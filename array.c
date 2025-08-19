9ff901b62bc1e3f3429ee464ef473bf44aa93043
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-19 07:16:23 +0600

    add 1st example of array in C

---
+++ array.c
@@ -0,0 +1,32 @@
+
+// ================================================
+//               * Language: C
+//               * Topic: Array
+// ===============================================
+
+// example 1️⃣ 
+
+#include <stdio.h>
+int main(){
+    int marks [3];
+    printf("Phy : ");
+    scanf("%d" , &marks[0]);
+
+    printf("Chemy : ");
+    scanf("%d" , &marks[1]);
+
+    printf("Bio : ");
+    scanf("%d" , &marks[2]);
+    printf(" Phy = %d  Chemy = %d  bio  = %d\n",  marks[0], marks[1], marks[2]);
+    return 0;
+}
+
+
+
+ 
+// #include <stdio.h>
+// int main(){
+    // return 0;
+// }
+
+// 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
