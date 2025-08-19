b4a4665eef2c1510e082c06c6c44e6d880fbd728
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-19 23:41:52 +0600

    add increment_Arithmatic of pointer in C

--- pointer-Arithmetic.c
+++ pointer-Arithmetic.c
@@ -1,4 +1,4 @@
-//  increment
+//  ================ increment
 // #include <stdio.h>
 // int main(){
 //  int age = 10;
@@ -12,14 +12,29 @@
 
 // =============   . decrement
 
+// #include <stdio.h>
+// int main(){
+// int age = 12;
+// int *ptr = &age;
+// *ptr++;
+// printf("%u\n", ptr);
+// *ptr--;
+// printf("%u\n", ptr);
+
+//     return 0;
+// }
+
+
+//  ============     float increment ============
 #include <stdio.h>
 int main(){
-int age = 12;
-int *ptr = &age;
+float price  = 20.00;
+ float *ptr = &price;
+ printf("Orginal byets = %u\n" , ptr );
 *ptr++;
-printf("%u\n", ptr);
+  printf("increment byets = %u\n" , ptr );
 *ptr--;
-printf("%u\n", ptr);
+  printf("decrement byets = %u\n" , ptr );
 
     return 0;
 }
