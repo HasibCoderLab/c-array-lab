5a0b27735764bc1b5c62d4307aa3d0740d3e612b
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-19 08:06:10 +0600

    solve 1st problem of Array in C

--- solve_problem.c
+++ solve_problem.c
@@ -1,5 +1,34 @@
 // ================================================
 //               * Language: C
 //               * Topic: Pointer
-//               *  solve practice Problem 
+//               *  solve practice Problem
 // =================================================
+
+//  // 👉👉  🔹🔹  Question 1️⃣ -  write a program to enter price of 3 times &
+//  print their  final cost with  gst
+
+#include <stdio.h>
+int main()
+{
+    float price[3];
+
+    printf("Enter3 price : ");
+
+    scanf("%f", &price[0]);
+    scanf("%f", &price[1]);
+    scanf("%f", &price[2]);
+
+    printf("Total price 1 = %f\n", price[0] + (0.18 * price[0]));
+    printf("Total price 2 = %f\n", price[1] + (0.18 * price[1]));
+    printf("Total  price 3 = %f\n", price[2] + (0.18 * price[2]));
+
+    return 0;
+}
+
+// #include <stdio.h>
+// int main(){
+
+// return 0;
+// }
+
+// 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
