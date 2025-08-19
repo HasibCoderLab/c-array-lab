f3cf0082fb6b1d0ef43df9f13e15f46ccead5448
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-19 23:28:32 +0600

    add decrement of pointer in C

--- pointer-Arithmetic.c
+++ pointer-Arithmetic.c
@@ -1,11 +1,24 @@
 //  increment
+// #include <stdio.h>
+// int main(){
+//  int age = 10;
+//  int *ptr = &age;
+// printf("%u\n" ,ptr);
+// *ptr++ ;
+// printf("%u\n" ,ptr);
+
+//     return 0;
+// }
+
+// =============   . decrement
+
 #include <stdio.h>
 int main(){
- int age = 10;
- int *ptr = &age;
-printf("%u\n" ,ptr);
-*ptr++ ;
-printf("%u\n" ,*ptr);
+int age = 12;
+int *ptr = &age;
+printf("%u\n", ptr);
+*ptr--;
+printf("%u\n", ptr);
 
     return 0;
 }
