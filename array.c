30240a6acc403e8c3c01416de7fa1f1b8f3ba114
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-20 08:33:16 +0600

    Traverse an array in C

--- array.c
+++ array.c
@@ -4,33 +4,56 @@
 //               * Topic: Array
 // ===============================================
 
-// example 1️⃣ 
+// example 1️⃣
+
+// #include <stdio.h>
+// int main(){
+//     int marks [3];
+//     printf("Phy : ");
+//     scanf("%d" , &marks[0]);
+
+//     printf("Chemy : ");
+//     scanf("%d" , &marks[1]);
+
+//     printf("Bio : ");
+//     scanf("%d" , &marks[2]);
+//     printf(" Phy = %d  Chemy = %d  bio  = %d\n",  marks[0], marks[1], marks[2]);
+//     return 0;
+// }
+
+//  ==================  Traverse an array ========
 
 #include <stdio.h>
-int main(){
-    int marks [3];
-    printf("Phy : ");
-    scanf("%d" , &marks[0]);
+int main()
+{
+    int NID[5];
+    // input
+    int *ptr = &NID[0];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d index = ", i);
+        scanf("%d", (ptr + i));
+    }
 
-    printf("Chemy : ");
-    scanf("%d" , &marks[1]);
+    // output
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d index =  %d\n", i, *(ptr + i));
+    }
 
-    printf("Bio : ");
-    scanf("%d" , &marks[2]);
-    printf(" Phy = %d  Chemy = %d  bio  = %d\n",  marks[0], marks[1], marks[2]);
     return 0;
 }
 
-
-
-
-
-
-
- 
 // #include <stdio.h>
 // int main(){
-    // return 0;
+
+// return 0;
+// }
+//
+
+// #include <stdio.h>
+// int main(){
+// return 0;
 // }
 
 // 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
