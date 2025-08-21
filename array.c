70145ec003e8bde394ddb81bd0b48e265d223f48
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-21 08:49:38 +0600

    example of 2D array in C

--- array.c
+++ array.c
@@ -106,6 +106,24 @@ void printN(int *arr , int n){
     printf("\n") ;
 }
 
+// 2D  array 
+
+
+#include <stdio.h>
+int main(){
+    int marks[2][3];
+    marks[0][0] = 98;
+    marks[0][1] = 99;
+    marks[0][3] = 100;
+
+
+    marks[1][0] = 95;
+    marks[1][1] = 96;
+    marks[1][3] = 10;
+return 0;
+}
+
+
 
 // #include <stdio.h>
 // int main(){
