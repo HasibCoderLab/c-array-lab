7f2259edaba9cb3a53737ec07794ddbbe115ae88
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-21 09:08:24 +0600

    solve 2nd problem of arry in  C

--- solve_problem.c
+++ solve_problem.c
@@ -29,25 +29,50 @@
 //  print their  final cost with  gst
 // ==========================  versoin 2.0 =======================
 
+// #include <stdio.h>
+// int main()
+// {
+//     float price[3];
+//     printf("Enter 1st price  :");
+//     scanf("%f", &price[0]);
+
+//     printf("Enter 2nd price  :");
+//     scanf("%f", &price[1]);
+
+//     printf("Enter 3rd price :");
+//     scanf("%f", &price[2]);
+
+//     printf("Total price 1 = %f\n", price[0] + (0.18 * price[0]));
+//     printf("Total price 2 = %f\n", price[1] + (0.18 * price[1]));
+//     printf("Total price 3 = %f\n", price[2] + (0.18 * price[2]));
+
+//     return 0;
+// }
+//   👉👉  🔹🔹  Question  2️⃣  write a function  to count the numbers of odd numbers in array
 #include <stdio.h>
+int count(int arr[], int n);
 int main()
 {
-    float price[3];
-    printf("Enter 1st price  :");
-    scanf("%f", &price[0]);
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
 
-    printf("Enter 2nd price  :");
-    scanf("%f", &price[1]);
-
-    printf("Enter 3rd price :");
-    scanf("%f", &price[2]);
-
-    printf("Total price 1 = %f\n", price[0] + (0.18 * price[0]));
-    printf("Total price 2 = %f\n", price[1] + (0.18 * price[1]));
-    printf("Total price 3 = %f\n", price[2] + (0.18 * price[2]));
+    printf("%d\n", count(arr, 7));
 
     return 0;
 }
+// Function Definition
+int count(int arr[], int n)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 // #include <stdio.h>
 // int main(){
 
